1da24d05210918b508d4ae30e0eda0ab9de4daff
Luis Pelaez <e.luis.pelaez@gmail.com>
2025-06-08 22:01:46 +0800

    real time visits update

--- assets/js/fetch-active-players.js
+++ assets/js/fetch-active-players.js
@@ -1,4 +1,4 @@
-function fetchAndDisplayActivePlayers() {
+function fetchAndDisplayActivePlayersAndVisits() {
     const robloxUrl = "https://games.roblox.com/v1/games?universeIds=1147304238,5768456460";
     const url = "https://corsproxy.io/?" + encodeURIComponent(robloxUrl);
     fetch(url)
@@ -9,17 +9,38 @@ function fetchAndDisplayActivePlayers() {
                 const totalActive = data.data.reduce((sum, game) => sum + (game.playing || 0), 0);
                 const elem = document.getElementById('active-players');
                 if (elem) elem.textContent = totalActive.toLocaleString();
+
+                //sum of visits from all games
+                const totalVisits = data.data.reduce((sum, game) => sum + (game.visits || 0), 0);
+                const visitsElem = document.getElementById('total-visits');
+                if (visitsElem) {
+                    let display = "";
+                    if (totalVisits >= 1_000_000_000) {
+                        display = totalVisits.toString().slice(0, 3) + "B+";
+                    } else if (totalVisits >= 1_000_000) {
+                        display = totalVisits.toString().slice(0, 3) + "M+";
+                    } else if (totalVisits >= 1_000) {
+                        display = totalVisits.toString().slice(0, 3) + "K+";
+                    } else {
+                        display = totalVisits.toString();
+                    }
+                    visitsElem.textContent = display;
+                }
             }
         })
         .catch(err => {
             const elem = document.getElementById('active-players');
             if (elem) elem.textContent = "N/A";
-            console.error("Failed to fetch active players:", err);
+
+            const visitsElem = document.getElementById('total-visits');
+            if(visitsElem) visitsElem.textContent = "N/A";
+
+            console.error("failed to fetch active players:", err);
         });
 }
 
 // call this after the hero component is loaded
 document.addEventListener("DOMContentLoaded", () => {
-    fetchAndDisplayActivePlayers();
-    setInterval(fetchAndDisplayActivePlayers, 60000); // refresh every 60 seconds
+    fetchAndDisplayActivePlayersAndVisits();
+    setInterval(fetchAndDisplayActivePlayersAndVisits, 60000); // refresh every 60 seconds
 });
